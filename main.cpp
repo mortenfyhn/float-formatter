1af19f46355854ace1ee652aff7a07eee630b522
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2018-10-06 17:52:42 +0200

    main: Change example matrix and add printing comparison

--- main.cpp
+++ main.cpp
@@ -1,49 +1,60 @@
-#include <cmath>
 #include <iostream>
-#include <random>
 #include <vector>
 
 #include "float-formatter.hpp"
 
-using std::cout;
-using std::vector;
-using matrix = vector<vector<double>>;
+using Matrix = std::vector<std::vector<double>>;
 
-void print(matrix const& m)
+Matrix generateMatrix()
 {
-    for (auto const& r : m)
+    Matrix matrix;
+
+    // Make a square matrix of zeros.
+    for (auto row = 0; row < 4; ++row)
     {
-        for (auto const& e : r)
-            cout << format(e) << ' ';
-        cout << '\n';
+        matrix.emplace_back();
+        for (auto col = 0; col < 4; ++col)
+        {
+            matrix[row].emplace_back();
+        }
     }
-    cout << '\n';
+
+    // Set ones on the diagonal.
+    for (auto i = 0; i < 4; ++i)
+        matrix[i][i] = 1;
+
+    // Set some illustration values in the upper diagonal.
+    matrix[1][0] = 1.2;
+    matrix[2][0] = 10.0 / 9.0;
+    matrix[3][0] = 1.23e11;
+    matrix[2][1] = -2.34e22;
+    matrix[3][1] = 3.45e-33;
+    matrix[3][2] = -4.56e-44;
+
+    return matrix;
+}
+
+template <typename Formatter> void print(Matrix const& matrix, Formatter format)
+{
+    for (auto const& row : matrix)
+    {
+        std::cout << "  ";
+        for (auto const& value : row)
+        {
+            std::cout << format(value) << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
 }
 
 int main()
 {
-    matrix m;
+    auto const matrix = generateMatrix();
 
-    m.emplace_back();
-    for (int i = -10; i < 0; ++i)
-        m[0].emplace_back(static_cast<double>(i));
+    std::cout << "Printed with operator<<():\n";
+    print(matrix, [](double x) { return x; });
 
-    m.emplace_back();
-    for (int i = 0; i < 10; ++i)
-        m[1].emplace_back(static_cast<double>(i));
-
-    std::default_random_engine g;
-    std::uniform_int_distribution<int> e(-10, 10);
-    std::uniform_real_distribution<double> s(-1, 1);
-
-    for (int i = 2; i < 10; ++i)
-    {
-        m.emplace_back();
-        for (int j = 0; j < 10; ++j)
-            m[i].emplace_back(s(g) * std::pow(10, e(g)));
-    }
-
-    print(m);
-
-    return 0;
+    std::cout << "Printed with formatter:\n";
+    print(matrix, [](double x) { return format(x, 6, true); });
 }
